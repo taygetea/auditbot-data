9f04b99639d6366c22b7062265f941837c406e33
taygetea <taygetea@gmail.com>
2015-04-14 13:38:43 -0700

    well it's certainly doing something kind of like what i wanted.

--- logparse.py
+++ logparse.py
@@ -1,13 +1,22 @@
 import json
+from collections import Counter
 
-wordlist = open("minidic-WORDLIST.txt")
+wordlist = open("minidic-WORDLIST.txt")  # first thousand
 rawlog = open("lesswrong_logs.json")
+print "Loading JSON..."
 logdict = json.load(rawlog)
-words = []
+logs = []
 users = set()
 for day in logdict.keys():
     for line in logdict[day]:
         if line[1] == u'PRIVMSG':
-            words.append(line)
+            logs.append(line)
             users.add(line[3])
-print words[:10]
+print "Removing common words..."
+uncommons = []
+for line in logs:
+    clean = line[4].split()  # Naive, will miss stuff
+    for elem in clean:
+        if elem not in wordlist:
+            uncommons.append(elem)
+print Counter(uncommons).most_common(1000)
